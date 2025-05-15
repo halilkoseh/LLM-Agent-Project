29a0759e4b0f99afaa14938204072e345c3e7996
Halil Köse <halilkose@alamet.ai>
2025-05-15 16:33:23 +0300

    s eklendi

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -13,7 +13,7 @@
 <body class="flex items-center justify-center h-screen bg-gray-100">
     <div class="w-full max-w-md p-8 space-y-6 bg-white rounded-lg shadow-lg">
         <!-- Title -->
-        <h2 class="text-3xl font-semibold text-center text-gray-800">LLM Platfrom</h2>
+        <h2 class="text-3xl font-semibold text-center text-gray-800">LLM Platfroms</h2>
 
         <!-- Session Status -->
         <div class="mb-4">
